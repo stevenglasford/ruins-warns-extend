a86797123a9f192638a1f11c311b9205c75138d0
stevenglasford <stevenglasford@gmail.com>
2020-04-29 19:23:28 -0500

    add new class for the new submission article

---
+++ src/app/submitArticle.ts
@@ -0,0 +1,10 @@
+export interface Article{
+  title: string;
+  author: string;
+  url: string;
+  body: string;
+  fakeness: string;
+  answererFakeness: string;
+  rating: number;
+  feedback: string;
+}
